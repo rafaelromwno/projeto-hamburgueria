83aec6ed0a61dbc867445e4f6f2728f415012017
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-10-24 16:04:22 -0300

    Configurando o EF Core

---
+++ WebApplicationHamburgueriaMvc/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationHamburgueriaMvc.Models;
+
+namespace WebApplicationHamburgueriaMvc.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {    
+            //Carrega as informações necessárias para configurar o DbContext
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
---
+++ WebApplicationHamburgueriaMvc/Models/Lanche.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplicationHamburgueriaMvc.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //Propriedade de Navegação -> Define o relacionamento entre as entidades (Convenção do Entity Framework Core)
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+
+
+    }
+}
