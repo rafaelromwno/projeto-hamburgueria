a4dc6d157f402a20a83ed8fd0bf09ddad1fd03d9
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-11-04 15:06:42 -0300

    Implementando os itens do carrinho de compras

---
+++ WebApplicationHamburgueriaMvc/Models/CarrinhoCompraItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationHamburgueriaMvc.Models
+{
+    [Table("CarrinhoCompraItens")] //redundante devido à existência do arquivo "context" que faz o controle das entidades
+    public class CarrinhoCompraItem
+    {
+        public int CarrinhoCompraItemId { get; set; }
+        public Lanche Lanche { get; set; }
+        public int Quantidade { get; set; }
+        [StringLength(200)]
+        public string CarrinhoCompraId { get; set; }
+    }
+}
