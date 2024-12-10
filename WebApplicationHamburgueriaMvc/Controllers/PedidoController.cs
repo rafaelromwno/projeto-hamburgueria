89b408ff76decc4bcd101b037fc2caf61b60b641
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-12-10 13:36:31 -0300

    finalizando a lógica de pedidos -> PedidoController.cs

--- WebApplicationHamburgueriaMvc/Controllers/PedidoController.cs
+++ WebApplicationHamburgueriaMvc/Controllers/PedidoController.cs
@@ -24,7 +24,48 @@ namespace WebApplicationHamburgueriaMvc.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            // obtém os itens do carrinho de compra do cliente
+            List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            //verifica se existem itens de pedido
+            if(_carrinhoCompra.CarrinhoCompraItens.Count() == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, que tal incluir um lanche?");
+            }
+
+            // calcular o total de itens e o total do pedido
+            foreach (CarrinhoCompraItem item in itens)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            // atribui os valores obtidos ao pedido 
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            // validar os dados do pedido
+            if (ModelState.IsValid)
+            {
+                // cria o pedido e seus detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pela preferência :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpar o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibir a view com os dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+
+            return View(pedido);
         }
     }
 }
