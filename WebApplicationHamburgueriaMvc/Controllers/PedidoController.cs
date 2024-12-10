cff09a6d393fdfb413a8dcf9d77dabfc800c7486
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-12-10 11:31:41 -0300

    criando o controlador PedidoController.cs + View

---
+++ WebApplicationHamburgueriaMvc/Controllers/PedidoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationHamburgueriaMvc.Models;
+using WebApplicationHamburgueriaMvc.Repositories.Interfaces;
+
+namespace WebApplicationHamburgueriaMvc.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
