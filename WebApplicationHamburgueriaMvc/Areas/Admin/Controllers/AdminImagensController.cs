59c83a2b8e035372e6b06f62de137bb7e8a39edb
Rafael Romano Silva <romanosilvarafael@gmail.com>
2024-12-12 09:10:40 -0300

    feat: gerenciamento de imagens implementado
    acesso somente à nível administrador!

---
+++ WebApplicationHamburgueriaMvc/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApplicationHamburgueriaMvc.Models;
+
+namespace WebApplicationHamburgueriaMvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminImagensController : Controller
+    {
+        private readonly ConfigurationImagens _configurationImagens;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AdminImagensController(IOptions<ConfigurationImagens> configurationImagens, IWebHostEnvironment webHostEnvironment)
+        {
+            _configurationImagens = configurationImagens.Value;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {            
+            return View();
+        }
+
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                ViewData["Erro"] = "Error: Arquivo(s) não selecionado(s)";
+                return View(ViewData);
+            }
+
+            if (files.Count > 10)
+            {
+                ViewData["Erro"] = "Error: Quantidade de arquivos excedeu o limite";
+                return View(ViewData);
+            }
+
+            long size = files.Sum(f => f.Length);
+            var filePathsName = new List<string>();
+            var filePath = Path.Combine(_webHostEnvironment.WebRootPath,
+                   _configurationImagens.NomePastaImagensProdutos);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.FileName.Contains(".jpg") || formFile.FileName.Contains(".gif") ||
+                         formFile.FileName.Contains(".png"))
+                {
+                    var fileNameWithPath = string.Concat(filePath, "\\", formFile.FileName);
+
+                    filePathsName.Add(fileNameWithPath);
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            // monta a ViewData que será exibida na view como resultado do envio 
+            ViewData["Resultado"] = $"{files.Count} arquivos foram enviados ao servidor, " +
+             $"com tamanho total de : {size} bytes";
+
+            ViewBag.Arquivos = filePathsName;
+
+            // retorna a viewdata
+            return View(ViewData);
+        }
+        public IActionResult GetImagens()
+        {
+            FileManagerModel model = new FileManagerModel();
+
+            var userImagesPath = Path.Combine(_webHostEnvironment.WebRootPath,
+                 _configurationImagens.NomePastaImagensProdutos);
+
+            DirectoryInfo dir = new DirectoryInfo(userImagesPath);
+            FileInfo[] files = dir.GetFiles();
+            model.PathImagensProduto = _configurationImagens.NomePastaImagensProdutos;
+
+            if (files.Length == 0)
+            {
+                ViewData["Erro"] = $"Nenhum arquivo encontrado na pasta {userImagesPath}";
+            }
+
+            model.Files = files;
+            return View(model);
+        }
+
+        public IActionResult Deletefile(string fname)
+        {
+            string _imagemDeleta = Path.Combine(_webHostEnvironment.WebRootPath,
+                _configurationImagens.NomePastaImagensProdutos + "\\", fname);
+
+            if ((System.IO.File.Exists(_imagemDeleta)))
+            {
+                System.IO.File.Delete(_imagemDeleta);
+                ViewData["Deletado"] = $"Arquivo(s) {_imagemDeleta} deletado com sucesso";
+            }
+            return View("index");
+        }
+    }
+}
